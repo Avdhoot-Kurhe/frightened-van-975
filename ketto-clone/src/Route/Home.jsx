85fe60c22c33dbc977c16b47f2cfc7c21e5faeee
Avdhoot-Kurhe <101327844+Avdhoot-Kurhe@users.noreply.github.com>
2022-08-25 22:12:56 +0530

    Day3

--- ketto-clone/src/Route/Home.jsx
+++ ketto-clone/src/Route/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import HomeImage from '../Components/Home/HomeImage';
 import Navbar from '../Components/Navbar/Navbar';
 
 function Home() {
     return (
         <div>
             <Navbar/>
+            <HomeImage />
         </div>
     );
 }
